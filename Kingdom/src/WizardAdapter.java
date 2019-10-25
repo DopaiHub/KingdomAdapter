1c7658863bf19450ef4d148b630dd330840cd758
JoshMolina <joshua.molina98@gmail.com>
2019-10-25 17:30:32 -0400

    Added code to the WizardAdapter to test if it works

--- Kingdom/src/WizardAdapter.java
+++ Kingdom/src/WizardAdapter.java
@@ -1,4 +1,20 @@
-
-public class WizardAdapter {
-
+/**
+ * 
+ * @author Joshua Molina
+ *
+ */
+public class WizardAdapter implements Warrior {
+	private Wizard wizard;
+	
+	public WizardAdapter(Wizard w) {
+		wizard = w;
+	}
+	
+	public void attack() {
+		wizard.performSpell();
+	}
+	
+	public void defend() {
+		wizard.heal();
+	}
 }
