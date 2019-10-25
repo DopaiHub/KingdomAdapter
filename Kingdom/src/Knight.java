c515eeb91480b4472e5b27448b8d15aa8c54b295
Adam Nguyen <dopaimail@gmail.com>
2019-10-25 14:57:56 -0400

    added comments

--- Kingdom/src/Knight.java
+++ Kingdom/src/Knight.java
@@ -1,9 +1,19 @@
-
+/**
+ * Knight class implements warrior and has attack and defend methods
+ * @author Adam Nguyen
+ *
+ */
 public class Knight implements Warrior{
+	/**
+	 * attack method prints out an attack
+	 */
 	public void attack()
 	{
 		System.out.println("Attacking: I'm bringing you down!");
 	}
+	/**
+	 * defend method prints out defense
+	 */
 	public void defend()
 	{
 		System.out.println("Defending: We must protect the castle");
--- Kingdom/src/Warrior.java
+++ Kingdom/src/Warrior.java
@@ -1,4 +1,8 @@
-
+/**
+ * Interface warrior forces methods attack and defend
+ * @author Adam Nguyen
+ *
+ */
 public interface Warrior {
 	public void attack();
 	public void defend();
