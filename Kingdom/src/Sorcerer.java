43c346101d021e101068f1e2fa452d934bd285d6
sem15 <sem15@email.sc.edu>
2019-10-25 15:28:21 -0400

    sorcerer

--- Kingdom/src/Sorcerer.java
+++ Kingdom/src/Sorcerer.java
@@ -1,4 +1,20 @@
+/*
+*@author Steven Maxwell
+*/
 
 public class Sorcerer {
+
+	/*
+	* performSpell method prints a spell
+	*/
+	public void performSpell() {
+		System.out.println("Casting Spell: I put a spell on you, and now you're gone");
+}
+	/*
+	* heal method prints a heal
+	*/
+	public void heal() {
+		System.out.println("Healing: You should all feel healthy again.");
+}
 
 }
